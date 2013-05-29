22ad5f6adacea8727f1f4c450ff0b4df50dc7563
Carlos Zarate <carmanuel.zarate@gmail.com>
2013-05-29 17:18:21 -0500

    cambios templates

--- src/Dicars/VentasBundle/Controller/DefaultController.php
+++ src/Dicars/VentasBundle/Controller/DefaultController.php
@@ -10,4 +10,8 @@ class DefaultController extends Controller
     {
         return $this->render('DicarsVentasBundle:Default:index.html.twig');
     }
+    public function clienteAction()
+    {
+    	return $this->render('DicarsVentasBundle:Default:cliente.html.twig');
+    }
 }
