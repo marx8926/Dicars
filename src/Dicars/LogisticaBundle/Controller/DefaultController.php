c249d2cf170d1675c43fc88be1c9d6e589b54146
Carlos Zarate <carmanuel.zarate@gmail.com>
2013-06-04 16:51:54 -0500

    cambios template

--- src/Dicars/LogisticaBundle/Controller/DefaultController.php
+++ src/Dicars/LogisticaBundle/Controller/DefaultController.php
@@ -14,4 +14,24 @@ class DefaultController extends Controller
     {
     	return $this->render('DicarsLogisticaBundle:Default:productos.html.twig');
     }
+    public function proveedoresAction()
+    {
+    	return $this->render('DicarsLogisticaBundle:Default:proveedores.html.twig');
+    }
+    public function almacenesAction()
+    {
+    	return $this->render('DicarsLogisticaBundle:Default:almacenes.html.twig');
+    }
+    public function cons_salidaproductosAction()
+    {
+    	return $this->render('DicarsLogisticaBundle:Default:salida_productos_consultar.html.twig');
+    }
+    public function reg_salidaproductosAction()
+    {
+    	return $this->render('DicarsLogisticaBundle:Default:salida_productos_registrar.html.twig');
+    }
+    public function cons_ingresoproductosAction()
+    {
+    	return $this->render('DicarsLogisticaBundle:Default:ingreso_productos_consultar.html.twig');
+    }
 }
