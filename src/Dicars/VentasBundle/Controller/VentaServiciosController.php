623fa4880fcc426af94f1a64de3058ba2ab6eb37
Garabateito <Garabateito@Sam>
2013-06-14 10:23:30 -0500

    RegistrarCliente

---
+++ src/Dicars/VentasBundle/Controller/VentaServiciosController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Dicars\VentasBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Normalizer\GetSetMethodNormalizer;
+
+class VentaServiciosController extends Controller{
+
+	
+	
+}
