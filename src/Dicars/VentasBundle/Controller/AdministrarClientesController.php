320670f987ca30c754a69731069514a3a21577e3
Garabateito <Garabateito@Sam>
2013-06-17 15:15:11 -0500

    VenZona

--- src/Dicars/VentasBundle/Controller/AdministrarClientesController.php
+++ src/Dicars/VentasBundle/Controller/AdministrarClientesController.php
@@ -1,6 +1,8 @@
 <?php
 namespace Dicars\VentasBundle\Controller;
+
 use Dicars\DataBundle\Entity\VenCliente;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\TransactionRequiredException;
@@ -13,86 +15,64 @@ class AdministrarClientesController extends Controller{
 		$form = $request->request->get('formulario');
 	
 		$datos = array();
-		parse_str($form,$datos);
+		parse_str($form, $datos);
 			
 		$Cliente_nombre = null;
 		$Cliente_apellido = null;
 		$Cliente_dni = null;
 		$Cliente_referencia = null;
-		$Cliente_direccion = null;
-		
+		$Cliente_direccion = null;		
 		$Cliente_zona = null;
 		
 		$Cliente_linea_op = null;
 		$Cliente_arc_credito = null;
-		$Cliente_ocup = null;					
+		$Cliente_ocup = null;				
 	
-		if ($form!=null){			
+		if ($form!=null){	
+					
 			$Cliente_nombre = $datos["nombres"];
-			$Cliente_apellido = $datos["apellidos"];
-				
-			$Producto_marca = $this->getDoctrine()
-			->getRepository('DicarsDataBundle:VenMarca')
-			->findOneBy(array('nmarcaId'  => 1));
-				
+			$Cliente_apellido = $datos["apellidos"];							
 			$Cliente_dni = $datos["dni"];
-			$Cliente_referencia = $datos["caracteristica"];
-			$Cliente_direccion = $datos["minimocontado"];
-			$Cliente_zona = $datos["minimocredito"];
-			$Cliente_linea_op = $datos["preciocosto"];
-			$Cliente_arc_credito = "AER1234";
-			$Cliente_ocup = "Hola soy el archivo";
+			$Cliente_referencia = $datos["referencia"];
+			$Cliente_direccion = $datos["direccion"];
+			
+			$Cliente_zona =  $this->getDoctrine()
+			->getRepository('DicarsDataBundle:VenZona')
+			->findOneBy(array('nzonaId' => 1));
+			/*
+			$Cliente_linea_op = 1200;
+			$Cliente_arc_credito = "ARC CREDITO";
+			$Cliente_ocup = "Hola soy la ocupacion";										
 				
-			$Producto_categoria =  $this->getDoctrine()
-			->getRepository('DicarsDataBundle:VenCategoria')
-			->findOneBy(array('ncategoriaId' => 1));
-				
-			$Producto_stock_min = 10;
-			$Producto_stock_max = 40;
-			$Producto_stock = 25;
-			$Producto_est = 1;
-			$Producto_porc_uti = 18;
-			$Producto_porc_uti_bruta = 20;
-				
-			$Producto = new Producto();
-			$Producto->setCproductotalla($Producto_talla);
-			$Producto->setCproductoserie($Producto_serie);
-			$Producto->setNproductomarca($Producto_marca);
-			$Producto->setNproductotipo($Producto_tipo);
-			$Producto->setCproductodesc($Producto_desc);
-			$Producto->setNproductopcontado($Producto_prec_contado);
-			$Producto->setNproductopcredito($Producto_prec_credito);
-			$Producto->setNproductopcosto($Producto_prec_costo);
-			$Producto->setCproductocodbarra($Producto_cod_barra);
-			$Producto->setCproductoimage($Producto_archivo);
-			$Producto->setNcategoria($Producto_categoria);
-			$Producto->setNproductostockmin($Producto_stock_min);
-			$Producto->setNproductostockmax($Producto_stock_max);
-			$Producto->setNproductosotck($Producto_stock);
-			$Producto->setCproductoest($Producto_est);
-			$Producto->setNproductoporcuti($Producto_porc_uti);
-			$Producto->setNproductoutibruta($Producto_porc_uti_bruta);
-				
+			$Cliente = new Cliente();
+			$Cliente->setBclientenom($Cliente_nombre);
+			$Cliente->setCclienteape($Cliente_apellido);
+			$Cliente->setCclientedni($Cliente_dni);
+			$Cliente->setCclienteref($Cliente_referencia);
+			$Cliente->setCclientecdir($Cliente_direccion);
+			$Cliente->setNzona($Cliente_zona);
+			$Cliente->setNclientelineaop($Cliente_linea_op);
+			$Cliente->setCclientearccredito($Cliente_arc_credito);
+			$Cliente->setCclienteocup($Cliente_ocup);
+						
+			
 			$em = $this->getDoctrine()->getEntityManager();
 			$this->getDoctrine()->getEntityManager()->beginTransaction();
 			try {
-				$em->persist($Producto);
+				$em->persist($Cliente);
 				$em->flush();
 			} catch (Exception $e) {
 				$this->getDoctrine()->getEntityManager()->rollback();
 				$this->getDoctrine()->getEntityManager()->close();
-				$return = array("responseCode"=>400, "greeting"=>"Bad");
-					
+				$return = array("responseCode"=>400, "greeting"=>"Bad");					
 				throw $e;
 			}
-			$this->getDoctrine()->getEntityManager()->commit();
-			$return = array("responseCode"=>200, "datos"=>$datos);
-				
+			$this->getDoctrine()->getEntityManager()->commit();*/
+			$return = array("responseCode"=>200, "datos"=>$datos);				
 		}
 		else {
 			$return = array("responseCode"=>400, "greeting"=>"Bad");
-		}
-	
+		}	
 		$return = json_encode($return);
 		return new Response($return,200,array('Content-Type'=>'application/json'));
 	}
