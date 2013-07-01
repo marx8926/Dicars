50f13b2a77947577e9d214385062794bf1bab85d
Garabateito <Garabateito@Sam>
2013-07-01 15:24:57 -0500

    AdministrarIngresoProductos

---
+++ src/Dicars/LogisticaBundle/Controller/AdministrarIngresoProdController.php
@@ -0,0 +1,106 @@
+<?php
+namespace Dicars\LogisticaBundle\Controller;
+
+use Dicars\DataBundle\Entity\LogDetingprod;
+
+use Dicars\DataBundle\Entity\LogIngprod;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\TransactionRequiredException;
+
+class AdministrarIngresoProdController extends Controller{
+	
+	public function RegistrarIngresoProdAction(){
+		$request = $this->get('request');
+		$form = $request->request->get('formulario');
+		$otherdata = $request->request->get('otherdata');
+	
+		$datos = array();
+		parse_str($form,$datos);
+	
+		$Registrante = null;
+		$Local = null;
+		$Serie = null;
+		$Numero = null;
+		$Motivo = null;
+		$SerieDoc = null;
+		$Fecha_reg = null;
+		$NumeroDoc = null;		
+		$Observacion = null;
+		$Estado = null;
+	
+		if ($form != null){
+				
+			$Registrante = $this->getDoctrine()
+			->getRepository('DicarsDataBundle:VenPersonal')
+			->findOneBy(array('npersonalId' => 1));
+				
+			$Local = $this->getDoctrine()
+			->getRepository('DicarsDataBundle:Local')
+			->findOneBy(array('nlocalId' => 1));
+				
+			$Serie ='QRETE';
+			$Numero ='12345678';
+			$Motivo = $datos['motivo'];
+			$SerieDoc = 'DOCFGTU';
+			$Fecha_reg = date_create_from_format('d/m/Y', $datos["fecharegistro"]);						
+			$NumeroDoc = 'DOC001';
+			$Observacion = $datos['observacion'];
+			$Estado = 'A';
+			/*
+			$IngProd = new  LogIngprod();
+			$IngProd -> setNpersonal($Registrante);
+			$IngProd -> setNlocal($Local);
+			$IngProd -> setCingprodserie($Serie);
+			$IngProd -> setCingprodnro($Numero);
+			$IngProd -> setNingprodmotivo($Motivo);
+			$IngProd -> setDingprodfecreg($Fecha_reg);
+			$IngProd -> setCingproddocnro($NumeroDoc);
+			$IngProd -> setCingprodobsv($Observacion);
+			$IngProd -> setCingprodest($Estado);
+	
+			$em = $this->getDoctrine()->getEntityManager();
+			$this->getDoctrine()->getEntityManager()->beginTransaction();
+			try {
+				$em->persist($IngProd);
+				$em->flush();
+	
+				foreach($otherdata as $key => $data){
+					$Producto = $this->getDoctrine()
+					->getRepository('DicarsDataBundle:Producto')
+					->findOneBy(array('nproductoId' => $data['id']));
+	
+					$DetalleIngProd = new LogDetingprod();
+					$DetalleIngProd -> setNingprod($IngProd);
+					$DetalleIngProd -> setNproducto($Producto);
+					$DetalleIngProd -> setNdetingprodcant($data['cantidad']);
+					$DetalleIngProd -> setNdetingprodprecunt($data['precio_uni']);
+					$DetalleIngProd -> setCdetsalprodest(45);
+						
+					$em->persist($DetalleIngProd);
+					$em->flush();
+				}
+	
+			} catch (Exception $e) {
+				$this->getDoctrine()->getEntityManager()->rollback();
+				$this->getDoctrine()->getEntityManager()->close();
+				$return = array("responseCode"=>400, "greeting"=>"Bad");
+					
+				throw $e;
+			}
+			$this->getDoctrine()->getEntityManager()->commit();
+			$em->clear();
+			*/
+			$return = array("responseCode"=>200, "datos"=>$datos);
+	
+		}
+		else {
+			$return = array("responseCode"=>400, "greeting"=>"Bad");
+		}
+	
+		$return = json_encode($return);
+		return new Response($return,200,array('Content-Type'=>'application/json'));
+	}
+	
+}
