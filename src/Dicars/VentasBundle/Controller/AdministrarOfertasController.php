6716476c9eb9eba14575c47d709b440b2c8e326a
Carlos Zarate <carmanuel.zarate@gmail.com>
2013-07-11 23:13:15 -0500

    casa

--- src/Dicars/VentasBundle/Controller/AdministrarOfertasController.php
+++ src/Dicars/VentasBundle/Controller/AdministrarOfertasController.php
@@ -1,6 +1,10 @@
 <?php
 namespace Dicars\VentasBundle\Controller;
 
+use Dicars\DataBundle\Entity\OfertaProducto;
+
+use Dicars\DataBundle\Entity\Oferta;
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\TransactionRequiredException;
@@ -10,14 +14,49 @@ class AdministrarOfertasController extends Controller{
 	public function RegistrarOfertaAction(){
 		$request = $this->get('request');
 		$form = $request->request->get('formulario');
+		$otherdata = $request->request->get('otherdata');
 		
 		$datos = array();
 		parse_str($form,$datos);
+		$OfertaDescripcion = null;
+		$OfertaPorcentaje = null;
+		$OfertaFechaVigente = null;
+		$OfertaFechaVencimiento = null;
+		$OfertaFechaEstado = null;
 		
 		if ($form!=null){
+			
+			$OfertaDescripcion = $datos['descripcion'];
+			$OfertaPorcentaje = $datos['descuento'];
+			$OfertaFechaVigente = date_create_from_format('d/m/Y',$datos["fecha_ini"]);
+			$OfertaFechaVencimiento = date_create_from_format('d/m/Y',$datos["fecha_fin"]);
+			$OfertaEstado = $datos['estado'];
+			
+			$Oferta = new Oferta();
+			$Oferta -> setCofertadesc($OfertaDescripcion);
+			$Oferta -> setCofertaest($OfertaEstado);
+			$Oferta -> setDofertafecvencto($OfertaFechaVencimiento);
+			$Oferta -> setDofertafecvigente($OfertaFechaVigente);
+			$Oferta -> setNofertaproc($OfertaPorcentaje);
+			
+			$em = $this->getDoctrine()->getEntityManager();
+			$this->getDoctrine()->getEntityManager()->beginTransaction();
 			try {
-				$em->persist($Empleado);
+				$em->persist($Oferta);
 				$em->flush();
+				
+				foreach($otherdata as $key => $data){
+					$Producto = $this->getDoctrine()
+					->getRepository('DicarsDataBundle:Producto')
+					->findOneBy(array('nproductoId' => $data['id']));
+						
+					$OfertaProducto = new OfertaProducto();
+					$OfertaProducto -> setNoferta($Oferta);
+					$OfertaProducto -> setNproducto($Producto);
+					$em->persist($OfertaProducto);
+					$em->flush();
+						
+				}
 			} catch (Exception $e) {
 				$this->getDoctrine()->getEntityManager()->rollback();
 				$this->getDoctrine()->getEntityManager()->close();
@@ -26,7 +65,7 @@ class AdministrarOfertasController extends Controller{
 			}
 			$this->getDoctrine()->getEntityManager()->commit();
 			$em->clear();
-			$return = array("responseCode"=>200, "datos"=>$datos);
+			$return = array("responseCode"=>200, "datos"=>$otherdata);
 		}
 		else {
 			$return = array("responseCode"=>400, "greeting"=>"Bad");
