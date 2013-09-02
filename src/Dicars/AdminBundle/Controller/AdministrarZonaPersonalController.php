3e5461673d7e5ebd944191e09b5b32939f6b4477
lavaldi <clindy.26@gmail.com>
2013-09-02 16:59:57 -0500

    CAMBIOS

--- src/Dicars/AdminBundle/Controller/AdministrarZonaPersonalController.php
+++ src/Dicars/AdminBundle/Controller/AdministrarZonaPersonalController.php
@@ -65,18 +65,18 @@ class AdministrarZonaPersonalController extends Controller{
 		$datos = array();
 		parse_str($form,$datos);
 	
-		$Personal = null;
+		$Zona = null;
 		
 		if ($form!=null){
-			$Personal = $this->getDoctrine()
-			->getRepository('DicarsDataBundle:VenPersonal')
-			->findOneBy(array('npersonalId'  => $datos["id_trabajador"]));
+			$Zona = $this->getDoctrine()
+			->getRepository('DicarsDataBundle:VenZona')
+			->findOneBy(array('nzonaId'  => $datos["idzona"]));
 				
 			$ZonaPersonal = $this->getDoctrine()
 			->getRepository('DicarsDataBundle:VenZonapersonal')
 			->findOneBy(array('nzonapersonalId' => $datos["idzonapersonal"]));
 			
-			$ZonaPersonal -> setNpersonal($Personal);
+			$ZonaPersonal -> setNzona($Zona);
 		
 			$em = $this->getDoctrine()->getEntityManager();
 			$em->beginTransaction();
